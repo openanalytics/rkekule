9ce598cb0b3a18d841edd50929c5c107f9485121
Daan Seynaeve <daan.seynaeve@openanalytics.eu>
2018-04-23 13:59:27 +0200

    ChemmineOB support in chemViewer widget

--- rkekule/R/chemViewer.R
+++ rkekule/R/chemViewer.R
@@ -9,7 +9,9 @@
 #' @param data moltable or cml
 #' @param width widget width
 #' @param height widget height
-#' @param chemicalFormat one of \code{c("mol", "cml"}
+#' @param chemicalFormat one of \code{c("mol", "cml", "smi", "inchi")}.
+#' If any of \code{c("smi", "inchi")}, the \code{\link[ChemmineOB]{convertFormat}}
+#' will be used to convert to mol before rendering.
 #' @param renderType one of \code{c("2D", "3D")}
 #' @param showTools show toolbar.
 #' @return htmlwidget
@@ -20,23 +22,38 @@ chemViewer <- function(
     data,
     width = NULL,
     height = NULL,
-    chemicalFormat = c("mol", "cml"),
+    chemicalFormat = c("mol", "cml", "smi", "inchi"),
     renderType = c("2D", "3D"),
     showTools = TRUE) {
   
-  chemicalFormat <- match.arg(chemicalFormat, c("mol", "cml"))
-  
-  renderType <- match.arg(renderType, c("2D", "3D"))
-  
-  settings <- list(
-      type = chemicalFormat,
-      renderType = renderType,
-      showTools = FALSE)
-  
-  x <- list(
-      data = data,
-      settings = settings)
-  
-  createWidget("rkekule", x, width = width, height = height)
+  if (chemicalFormat %in% c("smi", "inchi")) {
+    
+    if (!requireNamespace("ChemmineOB", quietly = TRUE)) {
+      stop("Package \"ChemmineOB\" needed for this function to work. Please install it.",
+          call. = FALSE)
+    }
+    
+    molData <- ChemmineOB::convertFormat(chemicalFormat, "mol", data)
+    
+    chemViewer(molData, width, height, "mol", renderType, showTools)
+    
+  } else {
+    
+    chemicalFormat <- match.arg(chemicalFormat, c("mol", "cml", "smi", "inchi"))
+    
+    renderType <- match.arg(renderType, c("2D", "3D"))
+    
+    settings <- list(
+        type = chemicalFormat,
+        renderType = renderType,
+        showTools = FALSE)
+    
+    x <- list(
+        data = data,
+        settings = settings)
+    
+    createWidget("rkekule", x, width = width, height = height)
+    
+  }
   
 }
